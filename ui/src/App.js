37a0b9921b45b946dd88fba0ceff4c80a3a26e28
Michael <cwithmichael@gmail.com>
2018-05-11 12:43:06 -0500

    adding shop by department grid to ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button, Grid, Row, Image, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import store from './store.jpg';
+import fruit from './fruit.jpg';
+import medicine from './medicine.jpg';
 import './App.css';
+import SearchBox from './SearchBox.js';
 
 class App extends Component {
 	constructor(props) {
 	    super(props);
 
 	    this.state = {
-	      products: [],
+        products: [],
+        departments: [{id: 1, title: "Produce", thumb: fruit}, {id: 2, title: "Groceries", thumb: store}, {id: 3, title: "Pharmacy", thumb: medicine}],
 	      isLoading: false
 	    };
 	  }
@@ -25,18 +31,25 @@ class App extends Component {
 	  
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div>
-        <h2>Product List</h2>
-        {this.state.products.map((product) =>
-          <li key={product.ID}>
-            {product.description}
-          </li>
-        )}
-      </div>
+      <Grid fluid={true}>
+      <Row className="header">
+        <h1 className="App-title">Welcome to Michael's Groceries Shop</h1>
+      </Row>
+      <Row className="searchBox">
+      <SearchBox/>
+      <Button>Search</Button>
+      </Row>
+      <Row className="departmentsGrid">
+      <h2>Shop By Department</h2>
+      {this.state.departments.map((department) =>
+        <Col xs={6} md={4} key={department.id}>
+          <h3>{department.title}</h3>
+          <Image src={department.thumb} className="App-logo" alt="store" responsive/>
+        </Col>
+      )}
+            </Row>
+      </Grid>
+        
       </div>
     );
   }
