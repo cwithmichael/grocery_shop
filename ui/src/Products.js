3dd8cdfb8c006c21f8c2a9eb250de0ae285d643e
Michael <cwithmichael@gmail.com>
2018-05-12 01:16:43 -0500

    formatting the code

--- ui/src/Products.js
+++ ui/src/Products.js
@@ -1,81 +1,84 @@
-import React, { Component } from 'react';
-import { Grid, Row, Image, Col, SplitButton, MenuItem} from 'react-bootstrap';
+import React, {Component} from 'react';
+import {
+    Grid,
+    Row,
+    Image,
+    Col,
+    SplitButton,
+    MenuItem
+} from 'react-bootstrap';
 import store from './store.jpg';
 import fruit from './fruit.jpg';
 import medicine from './medicine.jpg';
 import './App.css';
 
-
 export default class Product extends Component {
-	constructor(props) {
-	    super(props);
+    constructor(props) {
+        super(props);
 
-	    this.state = {
-        products: [],
-        image: null,
-	      isLoading: false
-      };
-      
-      this.onSort = this.onSort.bind(this)
+        this.state = {
+            products: [],
+            image: null,
+            isLoading: false
+        };
 
+        this.onSort = this
+            .onSort
+            .bind(this)
+
+    }
+
+    onSort(event, sortKey, direction) {
+        const data = this.state.products;
+        if (direction === "asc") 
+            data.sort((a, b) => a[sortKey] - b[sortKey])
+        else 
+            data.sort((a, b) => b[sortKey] - a[sortKey])
+        this.setState({data})
+    }
+
+    componentDidMount() {
+        this.setState({isLoading: true});
+        if (this.props.department.toLowerCase() === "produce") {
+            this.setState({image: fruit})
+        } else if (this.props.department.toLowerCase() === "grocery") {
+            this.setState({image: store})
+        } else {
+            this.setState({image: medicine})
+        }
+        fetch('http://localhost:8080/api/products/search/findByDepartment?department=' + this.props.department)
+            .then(response => response.json())
+            .then(data => this.setState({products: data._embedded.products, isLoading: false}));
     }
     
-    onSort(event, sortKey, direction){
-      /*
-      assuming your data is something like
-      [
-        {accountname:'foo', negotiatedcontractvalue:'bar'},
-        {accountname:'monkey', negotiatedcontractvalue:'spank'},
-        {accountname:'chicken', negotiatedcontractvalue:'dance'},
-      ]
-      */
-      const data = this.state.products;
-      if (direction==="asc")
-        data.sort((a,b) => a[sortKey]-b[sortKey])
-      else
-        data.sort((a,b) => b[sortKey]-a[sortKey])
-      this.setState({data})
+    render() {
+        if (this.state.isLoading) {
+            return <p>Loading...</p>;
+        }
+
+        return (
+            <div className="Products">
+                <SplitButton bsStyle={'default'} title={'Sort by'}>
+                    <MenuItem onSelect={e => this.onSort(e, 'price', 'asc')}>Price: Low to High</MenuItem>
+                    <MenuItem onSelect={e => this.onSort(e, 'price', 'desc')}>Price: High to Low</MenuItem>
+                </SplitButton>
+                <Grid fluid={true}>
+                    <Row className="productsGrid">
+                        {this
+                            .state
+                            .products
+                            .map((product) => <Col xs={6} md={4} key={product.pid}>
+                                <Image
+                                    src={this.state.image}
+                                    className="productThumb"
+                                    alt="product"
+                                    responsive/>
+                                <h3>{product.description}<br/>${Number(product.price).toFixed(2)}<br/>{product.xFor} {product.unit}</h3>
+                            </Col>)}
+                    </Row>
+                </Grid>
+
+            </div>
+        );
     }
-
-
-	componentDidMount() {
-		  this.setState({isLoading: true});
-          if (this.props.department.toLowerCase() === "produce") {
-              this.setState({image: fruit})
-          } else if (this.props.department.toLowerCase() === "grocery") {
-              this.setState({image: store})
-          } else {
-              this.setState({image: medicine})
-          }
-		  fetch('http://localhost:8080/api/products/search/findByDepartment?department=' + this.props.department)
-		    .then(response => response.json())
-		    .then(data => this.setState({products: data._embedded.products, isLoading: false}));
-		}
-  render() {
-	  if (this.state.isLoading) {
-	    return <p>Loading...</p>;
-	  }
-	  
-    return (
-      <div className="Products">
-        <SplitButton
-      bsStyle={'default'}
-      title={'Sort by'}>
-      <MenuItem onSelect={e => this.onSort(e, 'price', 'asc')}>Price: Low to High</MenuItem>
-      <MenuItem onSelect={e => this.onSort(e, 'price', 'desc')}>Price: High to Low</MenuItem>
-    </SplitButton>
-      <Grid fluid={true}>
-      <Row className="productsGrid">
-      {this.state.products.map((product) =>
-        <Col xs={6} md={4} key={product.pid}>
-          <Image src={this.state.image} className="productThumb" alt="product" responsive/>
-          <h3>{product.description}<br/>${Number(product.price).toFixed(2)}<br/>{product.xFor} {product.unit}</h3>
-        </Col>
-      )}
-                  </Row>
-      </Grid>
-        
-      </div>
-    );
-  }
 }
--- ui/src/SearchBox.js
+++ ui/src/SearchBox.js
@@ -1,46 +1,49 @@
-import React, { Component } from 'react';
-import { FormGroup, FormControl, HelpBlock } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {FormGroup, FormControl, HelpBlock} from 'react-bootstrap';
 
 export default class SearchBox extends Component {
-  constructor(props, context) {
-    super(props, context);
+    constructor(props, context) {
+        super(props, context);
 
-    this.handleChange = this.handleChange.bind(this);
+        this.handleChange = this
+            .handleChange
+            .bind(this);
 
-    this.state = {
-      value: ''
-    };
-  }
+        this.state = {
+            value: ''
+        };
+    }
 
-  getValidationState() {
-    const length = this.state.value.length;
-    if (length > 10) return 'success';
-    else if (length > 5) return 'warning';
-    else if (length > 0) return 'error';
-    return null;
-  }
+    getValidationState() {
+        const length = this.state.value.length;
+        if (length > 10) 
+            return 'success';
+        else if (length > 5) 
+            return 'warning';
+        else if (length > 0) 
+            return 'error';
+        return null;
+    }
 
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
+    handleChange(e) {
+        this.setState({value: e.target.value});
+    }
 
-  render() {
-    return (
-      <form>
-        <FormGroup
-          controlId="formBasicText"
-          validationState={this.getValidationState()}
-        >
-          <HelpBlock>Search for Products</HelpBlock>
-          <FormControl
-            type="text"
-            value={this.state.value}
-            placeholder=""
-            onChange={this.handleChange}
-          />
-          <FormControl.Feedback />
-        </FormGroup>
-      </form>
-    );
-  }
+    render() {
+        return (
+            <form>
+                <FormGroup
+                    controlId="formBasicText"
+                    validationState={this.getValidationState()}>
+                    <HelpBlock>Search for Products</HelpBlock>
+                    <FormControl
+                        type="text"
+                        value={this.state.value}
+                        placeholder=""
+                        onChange={this.handleChange}/>
+                    <FormControl.Feedback/>
+                </FormGroup>
+            </form>
+        );
+    }
 }
