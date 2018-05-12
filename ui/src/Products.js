64de14fa9c46d1777922859886f1263054c57990
Michael <cwithmichael@gmail.com>
2018-05-11 23:57:12 -0500

    adding placeholder images for products

--- ui/src/Products.js
+++ ui/src/Products.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Grid, Row, Image, Col} from 'react-bootstrap';
+import store from './store.jpg';
+import fruit from './fruit.jpg';
+import medicine from './medicine.jpg';
 import './App.css';
 
 
@@ -9,12 +12,19 @@ export default class Product extends Component {
 
 	    this.state = {
         products: [],
+        image: null,
 	      isLoading: false
 	    };
 	  }
 	componentDidMount() {
 		  this.setState({isLoading: true});
-
+          if (this.props.department.toLowerCase() === "produce") {
+              this.setState({image: fruit})
+          } else if (this.props.department.toLowerCase() === "grocery") {
+              this.setState({image: store})
+          } else {
+              this.setState({image: medicine})
+          }
 		  fetch('http://localhost:8080/api/products/search/findByDepartment?department=' + this.props.department)
 		    .then(response => response.json())
 		    .then(data => this.setState({products: data._embedded.products, isLoading: false}));
@@ -30,8 +40,10 @@ export default class Product extends Component {
       <Row className="productsGrid">
       {this.state.products.map((product) =>
         <Col xs={6} md={4} key={product.pid}>
+          <Image src={this.state.image} className="productThumb" alt="product" responsive/>
           <h3>{product.description}</h3>
-          <Image src={product.thumb} className="productThumb" alt="product" responsive/>
+          <h3>${product.price}</h3>
+          <h3>{product.xFor} {product.unit}</h3>
         </Col>
       )}
                   </Row>
