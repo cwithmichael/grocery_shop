99446b3de452f4c9683e056d49e83c00ba2978d5
Michael <cwithmichael@gmail.com>
2018-05-14 14:09:31 -0500

    fixing description in popover

--- ui/src/components/ProductPopover.js
+++ ui/src/components/ProductPopover.js
@@ -3,13 +3,14 @@ import {
     Popover
 } from 'react-bootstrap';
 import '../App.css';
+var _ = require('lodash');
 
 
 export default function ProductPopover(product) {
     return (<Popover id="popover-trigger-hover-focus" title="Product Details">
       <h5>{`Product Id: ${product.pid}`}</h5>
       <h5>{`Department: ${product.department}`}</h5>
-      <h5>{`Description: ${product.description}`}</h5>
+      <h5>{`Description: ${_.capitalize(product.description.trim().replace("_", " "))}`}</h5>
       <h5>{`Price: ${product.price}`}</h5>
       <h5>{`Cost: ${product.cost}`}</h5>
       <h5>{`Unit: ${product.unit}`}</h5>
